34520fc9b9b3652d01b95411b8dba0644858f64e
Yugo-Cartron <yugo.cartron@gmail.com>
2023-05-24 14:41:34 +0200

    Initialize svelte project

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,30 @@
-<script lang="ts">
-  import { Router } from "svelte-navigator";
-  import Navbar from "./components/Navbar.svelte";
-  import Routes from "./components/Routes.svelte";
-  import { setContext } from "svelte";
-  import { writable } from "svelte/store";
-  import { Context } from "./tools/context";
-  import type { CandyData } from "./types/candyData";
-  import "@candy-doc/ui/src/components/Topbar/Topbar";
-
-  const candyData = writable<CandyData>([]);
-  setContext(Context.CandyData, candyData);
-
-  async function fetchCandyData() {
-    const response = await fetch("/candy-data.json");
-    const bodyJSON: CandyData = await response.json();
-    $candyData = bodyJSON;
-  }
-  fetchCandyData();
+<script>
+	export let name;
 </script>
 
-<Router>
-  <div class="min-h-screen flex flex-col">
-    <candy-topbar></candy-topbar>
-    <div class="flex flex-1">
-      <div class="w-1/6">
-        <Navbar />
-      </div>
-      <div class="w-5/6">
-        <Routes />
-      </div>
-    </div>
-  </div>
-</Router>
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
